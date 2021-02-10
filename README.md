91027f99274522471a79a0e7c0beb79b06a067d8
John <jcwrightson@gmail.com>
2021-02-10 21:15:54 +0000

    restructure

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # IOT LED
-
-https://o07fzai121.execute-api.eu-west-1.amazonaws.com/Prod/
---
+++ device/main.cpp
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <iostream>
+#include "ArduinoJson.h"
+
+#define PIN LED_BUILTIN
+
+const char *API_KEY;
+const char *URL;
+const char *FINGER_PRINT;
+const int TICK_INTERVAL = 10000;
+
+const char *SSID;
+const char *WL_PASSWORD;
+
+HTTPClient https;
+
+void connect_to_wifi()
+{
+    WiFi.begin(SSID, WL_PASSWORD);
+
+    Serial.print("Connecting to wifi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    connect_to_wifi();
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop()
+{
+
+    https.begin(URL, FINGER_PRINT);
+    https.addHeader("x-api-key", API_KEY);
+
+    int code = https.GET();
+    String response = https.getString();
+
+    StaticJsonDocument<1024> doc;
+    deserializeJson(doc, response);
+
+    String author = doc["author"];
+
+    Serial.println(code);
+    Serial.println(author);
+
+    JsonArray stream = doc["stream"].as<JsonArray>();
+
+    for (JsonVariant row : stream)
+    {
+        JsonArray item = row.as<JsonArray>();
+        int state = item[0];
+        int duration = item[1];
+
+        Serial.print("Setting PIN: ");
+        Serial.println(item[0].as<int>());
+        digitalWrite(PIN, state == 1 ? LOW : HIGH);
+        Serial.print("Waiting for...: ");
+        Serial.print(duration);
+        Serial.print("ms");
+        delay(duration);
+    }
+
+    https.end();
+    delay(TICK_INTERVAL);
+}
